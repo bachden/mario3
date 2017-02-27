a51b9ef6f8f24c878a305dc8fbf0082f3bf12393
bachden <hoangbach.bk@gmail.com>
2017-02-27 19:04:37 +0700

    Fix bug classpath for TestZooKeeper

--- TestZooKeeper/src/com/mario/test/zookeeper/TestZooKeeperLeaderElection.java
+++ TestZooKeeper/src/com/mario/test/zookeeper/TestZooKeeperLeaderElection.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.NodeExistsException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.Watcher.Event.EventType;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.KeeperException.NodeExistsException;
 import org.apache.zookeeper.data.Stat;
 
-import nhb.common.data.PuObjectRO;
-import nhb.mario3.entity.impl.BaseMessageHandler;
-import nhb.mario3.schedule.ScheduledCallback;
+import com.mario.entity.impl.BaseMessageHandler;
+import com.mario.schedule.ScheduledCallback;
+import com.nhb.common.data.PuObjectRO;
 
 public class TestZooKeeperLeaderElection extends BaseMessageHandler {
 
